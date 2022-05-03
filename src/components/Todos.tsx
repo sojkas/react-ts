0cddc8d3d0ef9a8d0946256378a60a6b74205291
Stepan Sojka <sojka.stepan@gmail.com>
2022-05-03 17:33:26 +0200

    jednoduche todo na vyzkouseni reactu a typescriptu

---
+++ src/components/Todos.tsx
@@ -0,0 +1,15 @@
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  return (
+    <ul className={classes.todos}>
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text ={item.text} />
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
